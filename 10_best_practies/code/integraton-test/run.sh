e8c366c16ca9079a63d46b5ed3325bb79a5df4fc
Thar Htet San <tharhtet1234@gmail.com>
2023-10-17 01:13:44 +0700

    add docker-compose

--- 10_best_practies/code/integraton-test/run.sh
+++ 10_best_practies/code/integraton-test/run.sh
@@ -3,19 +3,13 @@ cd "$(dirname "$0")"
 
 
 LOCAL_TAG=`date +"%Y-%m-%d-%H-%M"`
-LOCAL_IMAGE_NAME="stream-model-duration:${LOCAL_TAG}"
-    
+export LOCAL_IMAGE_NAME="stream-model-duration:${LOCAL_TAG}"
 
-docker build -t ${LOCAL_IMAGE_NAME} .
+echo "LOCAL_IMAGE_NAME is not set, building a new image with tag ${LOCAL_IMAGE_NAME}"
+docker build -t ${LOCAL_IMAGE_NAME} ..
 
-docker run -it --rm \
-    -p 8080:8080 \
-    -e PREDICTIONS_STREAM_NAME="ride_predictions" \
-    -e RUN_ID="Test123" \
-    -e MODEL_LOCATION="/app/model" \
-    -e TEST_RUN="True" \
-    -e AWS_DEFAULT_REGION="eu-west-1" \
-    -v $(pwd)/model:/app/model \
-    ${LOCAL_IMAGE_NAME}
+docker-compose up -d
 
 pipenv run python test-docker.py
+
+docker-compsoe down
